886b17ed4e23ef62d28467d14d6d05f793f06272
rizwanullah313 <ru323091@gmail.com>
2021-10-24 12:15:09 +0000

    Pass data nad dynamic

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profile</title>
-</head>
-<body>
+@extends("layout")
+@section("title")
+Profle Paagee
+@endsection
+@section("contents")
     <h1>Mr Profile</h1>
     <h4>{{$myId}} ==== {{$myEmail}}</h4>
     @foreach($info as $user)
@@ -18,5 +14,4 @@
 @else
 <p>You are yonger</p>
 @endif
-</body>
-</html>
+@endsection
