41fdc5ca91623666239e4f63e6745127e7044739
rizwanullah313 <ru323091@gmail.com>
2021-10-24 11:32:18 +0000

    some basic query and pass paramters in function

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get("/profile/{id}/{mi}", function($id,$mi){
+    // return view('profile');
+    return $id+$mi;
+});
+// Route::get("/profile", function()
+// {
+//   return "Rizwan";
+// });
+
+// oute class k ander aik static method hy get
+// forward slash home route
+// per closure function hy jis me hy welcome
